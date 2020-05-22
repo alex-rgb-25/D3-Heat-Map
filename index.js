45c2684b07c7e256e85f91dfe43df500b94d2d42
Alexandru Simionescu <alexandru.simionescu.94@gmail.com>
2020-05-22 09:23:09 +0300

    finished

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var app=express();
+var d3=require("d3");
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get("/",function(req,res){
+    res.render("home.ejs");
+
+})
+
+
+app.listen(process.env.PORT || 3000, function(req,res){
+    console.log("Server is running!");
+})
